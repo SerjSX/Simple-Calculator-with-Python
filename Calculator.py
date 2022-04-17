1ce5f477031f536e5d8e70a4a340b32cc268e50b
Serj <74871847+SerjSX@users.noreply.github.com>
2022-04-17 14:19:57 +0300

    Improved formatting
    Space, more clear texts and error results, changed calculation methods to sign typing.

--- Calculator.py
+++ Calculator.py
@@ -6,7 +6,7 @@ print("")
 
 def calculatorSession():
     print("Please insert the first number you will calculate. If you want to exit just type exit instead.")
-    number1 = input("N1: ")
+    number1 = input("First number: ")
 
     if number1 != 'exit':
         try:
@@ -20,7 +20,7 @@ def calculatorSession():
 
     print("")
     print("Now insert the number you want to calculate it with. If you want to exit just type exit instead.")
-    number2 = input("N2: ")
+    number2 = input("Second number: ")
 
     if number2 != 'exit':
         try:
@@ -33,33 +33,40 @@ def calculatorSession():
         exit()
 
     print("")
-    print("Now you will type the calculation method you want to use. There are three available: addition, subtraction, multiplication and division.")
+    print("Now you will type the calculation method you want to use. There are three available...")
+    print("")
+    print("> Addition: +")
+    print("> Subtraction: -")
+    print("> Multiplication: *")
+    print("> Division: /")
+    print("")
     print("The first number you chose is " + number1 + ", the second number you chose is " + number2 + ".")
-    calculation = input("What calculation method would you like to calculate it with? ")
+    print("")
+    calculation = input("What calculation method would you like to calculate it with? Type the sign (+/-/*/'/'): ")
 
     def calculationMethod(calculation, number1, number2):
-        if (calculation == 'addition' and number1I != 'null' and number2I != 'null'):
+        if (calculation == '+' and number1I != 'null' and number2I != 'null'):
             additionCalc = number1 + number2
             print("")
             print("--")
             print(additionCalc)
             print("--")
             print("")
-        elif (calculation == 'subtraction' and number1I != 'null' and number2I != 'null'):
+        elif (calculation == '-' and number1I != 'null' and number2I != 'null'):
             subtractionCalc = number1 - number2
             print("")
             print("--")
             print(subtractionCalc)
             print("--")
             print("")
-        elif (calculation == 'multiplication' and number1I != 'null' and number2I != 'null'):
+        elif (calculation == '*' and number1I != 'null' and number2I != 'null'):
             multiplyCalc = number1 * number2
             print("")
             print("--")
             print(multiplyCalc)
             print("--")
             print("")
-        elif (calculation == 'division' and number2 != 0 and number1I != 'null' and number2I != 'null'):
+        elif (calculation == '/' and number2 != 0 and number1I != 'null' and number2I != 'null'):
             divideCalc = number1 / number2
             print("")
             print("--")
@@ -69,7 +76,9 @@ def calculatorSession():
         else:
             print("")
             print("--")
-            print("This calculation is invalid. Most probably you tried dividing with 0, which results math error, not possible.")
+            print("This calculation is invalid.")
+            print("Possible reason 1: you tried dividing with 0. That's not possible and it results math error.")
+            print("Possible reason 2: you typed the word for the calculation method instead of the sign. Please use the sign as it's shorter and simpler.")
             print("--")
             print("")
 
